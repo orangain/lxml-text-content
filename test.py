6feac0bbf6e070722fd8922629a4849ab5fc2e11
orangain <orangain@gmail.com>
2013-11-04 01:30:07 +0900

    Add UlLiSpaceTestCase

--- test.py
+++ test.py
@@ -35,6 +35,36 @@ class UlLiTestCase(unittest.TestCase):
         self.assertEquals(ul.tail, '\n')
 
 
+class UlLiSpaceTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.doc = lxml.html.fromstring('''
+<html>
+<ul>
+ <li>1st</li>
+<li>2nd</li>
+</ul>
+</html>''')
+
+    def test_text_content(self):
+        ul = self.doc.xpath('//ul')[0]
+        self.assertEquals(ul.text_content(), '\n 1st\n2nd\n')
+
+    def test_to_string(self):
+        ul = self.doc.xpath('//ul')[0]
+        self.assertEquals(
+            lxml.html.tostring(ul),
+            '<ul>\n <li>1st</li>\n<li>2nd</li>\n</ul>\n')
+
+    def test_text(self):
+        ul = self.doc.xpath('//ul')[0]
+        self.assertEquals(ul.text, '\n ')
+
+    def test_tail(self):
+        ul = self.doc.xpath('//ul')[0]
+        self.assertEquals(ul.tail, '\n')
+
+
 class InvalidUlLiTestCase(unittest.TestCase):
 
     def setUp(self):
