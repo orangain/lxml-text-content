6e616ceb8d0dc737b3415fe59e425fb5f7920973
orangain <orangain@gmail.com>
2013-11-04 00:46:34 +0900

    Add tostring test

--- test.py
+++ test.py
@@ -7,9 +7,8 @@ import lxml.html
 
 class LxmlTestCase(unittest.TestCase):
 
-    def test_text_content(self):
-
-        doc = lxml.html.fromstring('''
+    def setUp(self):
+        self.doc = lxml.html.fromstring('''
 <html>
 <ul id="main">
 <li>1. AAA</li>
@@ -21,11 +20,17 @@ class LxmlTestCase(unittest.TestCase):
 </ul>
 </html>''')
 
-        self.assertEquals(doc.xpath('//ul')[0].text_content().strip(), '''1. AAA
+    def test_text_content(self):
+        self.assertEquals(self.doc.xpath('//ul')[0].text_content().strip(), '''1. AAA
 2. BBB
 3. CCC
 3-1. DDD
 3-2. EEE''')
+
+    def test_to_string(self):
+        self.assertEquals(
+            lxml.html.tostring(self.doc.xpath('//ul')[0]),
+            '<ul id="main">\n<li>1. AAA</li>\n<li>2. BBB</li>\n<li>3. CCC<ul>\n<li>3-1. DDD</li>\n<li>3-2. EEE</li>\n</ul></li>\n</ul>\n')
 
 
 if __name__ == '__main__':
