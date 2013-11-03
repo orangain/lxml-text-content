38a7872f61cda40c909f9efc9b92377fee7d0743
orangain <orangain@gmail.com>
2013-11-04 01:22:29 +0900

    Add div/span test case

--- test.py
+++ test.py
@@ -5,7 +5,37 @@ import unittest
 import lxml.html
 
 
-class LxmlTestCase(unittest.TestCase):
+class UlLiTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.doc = lxml.html.fromstring('''
+<html>
+<ul>
+<li>1st</li>
+<li>2nd</li>
+</ul>
+</html>''')
+
+    def test_text_content(self):
+        ul = self.doc.xpath('//ul')[0]
+        self.assertEquals(ul.text_content(), '\n1st\n2nd\n')
+
+    def test_to_string(self):
+        ul = self.doc.xpath('//ul')[0]
+        self.assertEquals(
+            lxml.html.tostring(ul),
+            '<ul>\n<li>1st</li>\n<li>2nd</li>\n</ul>\n')
+
+    def test_text(self):
+        ul = self.doc.xpath('//ul')[0]
+        self.assertEquals(ul.text, '\n')
+
+    def test_tail(self):
+        ul = self.doc.xpath('//ul')[0]
+        self.assertEquals(ul.tail, '\n')
+
+
+class DivSpanTestCase(unittest.TestCase):
 
     def setUp(self):
         self.doc = lxml.html.fromstring('''
